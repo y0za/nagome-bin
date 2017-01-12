e0f05a045044f847fd1565a40f7451bf02361cb3
yoza <yoza.is12s@gmail.com>
2017-01-12 23:06:12 +0900

    Add conditional branch by version to test.js

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 import test from 'ava';
 import pify from 'pify';
+import compareVersions from 'compare-versions'
 import { execFile } from 'child_process';
 import { version } from './package.json';
 
@@ -7,7 +8,13 @@ test('check nagome version', async t => {
   const output = await pify(execFile)('./cli.js', ['-v']);
   const columns = output.trim().split(/\s+/);
   t.true(columns.length >= 2);
-  t.is(columns[1], version);
+  if (compareVersions(version, '0.0.3') === -1) {
+    // 'Nagome   0.0.1' is output by nagome version 0.0.2
+    // https://github.com/diginatu/nagome/blob/v0.0.2/main.go#L13
+    t.is(columns[1], '0.0.1');
+  } else {
+    t.is(columns[1], `v${version}`);
+  }
 })
 
 
